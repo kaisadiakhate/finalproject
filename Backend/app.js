02b56c5d0e8d1f88cf7f4c8fa73ef804b4d565bf
kaisadiakhate <kaisa.diakhate@awacademy.training>
2022-10-31 15:46:31 +0200

    added get method to all foods

--- Backend/app.js
+++ Backend/app.js
@@ -2,10 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 8080;
-const {} = require("./database");
+const { readFoods } = require("./database");
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/", async (req, res) => {
+  const food = await readFoods();
+  const response = { data: food };
+  res.send(response);
+});
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
--- Backend/database.js
+++ Backend/database.js
@@ -1,24 +1,25 @@
-const dotenv = require("dotenv")
-dotenv.config()
-const { Pool } = require("pg")
+const dotenv = require("dotenv");
+dotenv.config();
+const { Pool } = require("pg");
 const pool = new Pool({
   user: process.env.user,
   host: process.env.host,
   database: process.env.database,
   password: process.env.password,
   port: 5432,
-})
+});
 
-async function main() {
-  const client = await pool.connect()
+async function readFoods() {
+  const client = await pool.connect();
   try {
-    const res = await client.query("SELECT * from foods;")
-    console.log("OK:", res.rows[0])
+    const res = await client.query(
+      "SELECT foods.foodid, foodname,eufdname,bestloc from foods join foodnutrientvalues on foodnutrientvalues.foodid = foods.foodid where eufdname = 'ENERC' or eufdname = 'FAT' or eufdname = 'CHOAVL' or eufdname = 'PROT' or eufdname = 'SUGAR' order by foodname"
+    );
+    console.log("OK:", res.rows[0]);
+    return res.rows;
   } catch (err) {
-    console.log(err?.stack)
+    console.log(err?.stack);
   }
-  client.release()
-  pool.end()
 }
-main()
-module.exports = {}
+
+module.exports = { readFoods };
