e4aaa8d04378c803c453c51b1c13739dc18e1b67
jantsigit <jan.lohva@gmail.com>
2022-10-31 13:28:42 +0200

    databasea kokeiltu -jan

--- Backend/database.js
+++ Backend/database.js
@@ -1,24 +1,24 @@
-const dotenv = require("dotenv");
-dotenv.config();
-const { Pool } = require("pg");
+const dotenv = require("dotenv")
+dotenv.config()
+const { Pool } = require("pg")
 const pool = new Pool({
   user: process.env.user,
   host: process.env.host,
   database: process.env.database,
   password: process.env.password,
   port: 5432,
-});
+})
 
 async function main() {
-  const client = await pool.connect();
+  const client = await pool.connect()
   try {
-    const res = await client.query("SELECT NOW();");
-    console.log("OK:", res.rows[0]);
+    const res = await client.query("SELECT * from foods;")
+    console.log("OK:", res.rows[0])
   } catch (err) {
-    console.log(err?.stack);
+    console.log(err?.stack)
   }
-  client.release();
-  pool.end();
+  client.release()
+  pool.end()
 }
-main();
-module.exports = {};
+main()
+module.exports = {}
