9fc8e91a2ae58f86218cbe734291b96da528a77c
Jlndl <jaakko.lundell@gmail.com>
2022-10-31 11:39:47 +0200

    added basic folders and filed for the project

---
+++ Backend/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 8080;
+const {} = require("./database");
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
