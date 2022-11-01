1a2ebfa324e9cb5f3d895d3cd4ffffb06e5c8e3a
kaisadiakhate <kaisa.diakhate@awacademy.training>
2022-11-01 10:45:21 +0200

    added readFoodId function

--- Backend/app.js
+++ Backend/app.js
@@ -1,18 +1,34 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 8080;
-const { readFoods } = require("./database");
+const port = process.env.PORT || 3000;
+const { readFoods, readFoodId } = require("./database");
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-  const food = await readFoods();
-  const response = { data: food };
-  res.send(response);
+  try {
+    const food = await readFoods();
+    const response = { data: food };
+    res.send(response);
+  } catch (err) {
+    console.log(err?.stack);
+  }
 });
 
+app.get("/:foodid", async (req, res) => {
+  const foodid = req.params.foodid;
+  console.log(`Asked for foodid ${foodid}`);
+  res.json(await readFoodId(foodid));
+});
+
+/*
+app.post("/search", async (req, res) => {
+  await createFood(req.body);
+  res.sendStatus(200);
+});
+*/
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
--- Backend/database.js
+++ Backend/database.js
@@ -1,5 +1,6 @@
 const dotenv = require("dotenv");
 dotenv.config();
+
 const { Pool } = require("pg");
 const pool = new Pool({
   user: process.env.user,
@@ -10,9 +11,8 @@ const pool = new Pool({
 });
 
 async function readFoods() {
-  const client = await pool.connect();
   try {
-    const res = await client.query(
+    const res = await pool.query(
       "SELECT foods.foodid, foodname,eufdname,bestloc from foods join foodnutrientvalues on foodnutrientvalues.foodid = foods.foodid where eufdname = 'ENERC' or eufdname = 'FAT' or eufdname = 'CHOAVL' or eufdname = 'PROT' or eufdname = 'SUGAR' order by foodname"
     );
     console.log("OK:", res.rows[0]);
@@ -22,4 +22,32 @@ async function readFoods() {
   }
 }
 
-module.exports = { readFoods };
+async function readFoodId(foodid) {
+  try {
+    const res = await pool.query(`SELECT * FROM foods WHERE foodid=$1`, [
+      foodid,
+    ]);
+    console.log(res.rows);
+    return res;
+  } catch (err) {
+    console.log(err?.stack);
+  }
+}
+
+/*
+async function createFood(foodname) {
+  console.log(foodname);
+  let today = new Date();
+  await pool.query(
+    `INSERT INTO meals () VALUES ($1, $2, $3, $4, $5, $6)`,
+    [
+      meals.
+      today.toISOString(),
+      meals.,
+      meals.,
+    ]
+  );
+}
+*/
+
+module.exports = { readFoods, readFoodId };
