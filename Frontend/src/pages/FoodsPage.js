17c40b48040708225bbd47adb8d1f206f1618de5
Risto-Matti Pulkkinen <rm.pulkkinen@gmail.com>
2022-11-03 13:31:18 +0200

    data.data

--- Frontend/src/pages/FoodsPage.js
+++ Frontend/src/pages/FoodsPage.js
@@ -10,7 +10,7 @@ export default function FoodsPage() {
         return response.json();
       })
       .then((data) => {
-        setFoods(data);
+        setFoods(data.data);
         console.log("Data from api:", data);
       });
   };
