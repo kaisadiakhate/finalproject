a036081c675ac8776930ee9bfde4339f178e963c
jantsigit <jan.lohva@gmail.com>
2022-11-04 09:54:36 +0200

    meals taulukko lisätty

--- Frontend/src/pages/MealsPage.js
+++ Frontend/src/pages/MealsPage.js
@@ -1,28 +1,27 @@
-import React, { useState, useEffect } from "react";
-import BasicCard from "../components/BasicCard";
+import React, { useState, useEffect } from "react"
+import MealsTable from "../components/MealsTable"
 
-//import PriceNameTable from "../components/PriceNameTable";
 export default function MealsPage() {
-  const [data, setMeals] = useState([]);
+  const [data, setMeals] = useState([])
 
   const fetchData = () => {
     fetch("http://localhost:4000/meals")
       .then((response) => {
-        return response.json();
+        return response.json()
       })
       .then((data) => {
-        setMeals(data.data);
-        console.log("Data from api:", data);
-      });
-  };
+        setMeals(data.data)
+        console.log("Data from api:", data)
+      })
+  }
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
   return (
     <div>
-      <BasicCard />
+      <MealsTable rows={data} />
     </div>
-  );
+  )
 }
