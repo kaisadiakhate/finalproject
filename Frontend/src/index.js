0d663014c4e95e4ac158b15f752cdab6aed38bf9
jantsigit <jan.lohva@gmail.com>
2022-11-07 11:32:09 +0200

    footer komponentti lisätty

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
+import Footer from "./components/Footer"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
     <div>
       <App />
+      <Footer />
     </div>
   </React.StrictMode>
 )
