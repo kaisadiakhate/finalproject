ccc2f5f15e55eaffd479d1e519875a7a81488a95
jantsigit <jan.lohva@gmail.com>
2022-11-08 14:08:22 +0200

    addmealpage muokattu

--- Frontend/src/components/AddMealTable.js
+++ Frontend/src/components/AddMealTable.js
@@ -1,80 +1,41 @@
-import React, { useState } from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import Checkbox from "@mui/material/Checkbox";
-import { useForm } from "react-hook-form";
+import React, { useState } from "react"
+import Table from "@mui/material/Table"
+import TableBody from "@mui/material/TableBody"
+import TableCell from "@mui/material/TableCell"
+import TableContainer from "@mui/material/TableContainer"
+import TableHead from "@mui/material/TableHead"
+import TableRow from "@mui/material/TableRow"
+import Paper from "@mui/material/Paper"
+import Checkbox from "@mui/material/Checkbox"
+import { useForm } from "react-hook-form"
 
 export default function AddMealTable(props) {
-  const { register, handleSubmit } = useForm();
-  const [selectedFood, setSelectedFood] = React.useState([]);
+  const { register, handleSubmit } = useForm()
+  const [selectedFood, setSelectedFood] = React.useState([])
 
   const onSubmit = (data) => {
-    console.log("Received on submit:", data);
-  };
+    console.log("Received on submit:", data)
+  }
 
   function checkFood(food) {
     if (selectedFood.find((f) => food.foodname === f.foodname)) {
-      setSelectedFood(selectedFood.filter((f) => f.foodname !== food.foodname));
+      setSelectedFood(selectedFood.filter((f) => f.foodname !== food.foodname))
     } else {
-      setSelectedFood([...selectedFood, food]);
+      setSelectedFood([...selectedFood, food])
     }
-    console.log(selectedFood);
+    console.log(selectedFood)
   }
   return (
     <>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <Table></Table>
-              <TableCell>Foodname (100g serving)</TableCell>
-              <TableCell align="right">Kokonaisenergia(kJ)</TableCell>
-              <TableCell align="right">Proteiini(g)</TableCell>
-              <TableCell align="right">Hiilarit(g)</TableCell>
-              <TableCell align="right">Rasva(g)</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {props.rows.map((row) => (
-              <TableRow
-                key={row.foodname}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell padding="checkbox">
-                  <Checkbox
-                    onChange={() => {
-                      checkFood(row);
-                    }}
-                    color="primary"
-                  />
-                </TableCell>
-                <TableCell component="th" scope="row">
-                  {row.foodname[0].toUpperCase() +
-                    row.foodname.slice(1).toLowerCase()}
-                </TableCell>
-                <TableCell align="right">{row.ENERC}</TableCell>
-                <TableCell align="right">{row.PROT}</TableCell>
-                <TableCell align="right">{row.CHOAVL}</TableCell>
-                <TableCell align="right">{row.FAT}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
       {selectedFood.length > 0 && (
         <form
           onSubmit={handleSubmit((data) => {
-            console.log(data);
+            console.log(data)
             fetch("http://localhost:4000/addmeal", {
               method: "POST",
               headers: { "Content-Type": "application/json" },
               body: JSON.stringify(data),
-            });
+            })
           })}
         >
           <p>
@@ -108,13 +69,52 @@ export default function AddMealTable(props) {
                       placeholder="Quantity"
                     ></input>
                   </li>
-                );
+                )
               })}
             </ul>
-            <input type="submit" value="Lisää" />
+            <input type="submit" value="Lisää valintasi päivän aterioihin" />
           </p>
         </form>
       )}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <Table></Table>
+              <TableCell>Foodname (100g serving)</TableCell>
+              <TableCell align="right">Kokonaisenergia(kJ)</TableCell>
+              <TableCell align="right">Proteiini(g)</TableCell>
+              <TableCell align="right">Hiilarit(g)</TableCell>
+              <TableCell align="right">Rasva(g)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {props.rows.map((row) => (
+              <TableRow
+                key={row.foodname}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell padding="checkbox">
+                  <Checkbox
+                    onChange={() => {
+                      checkFood(row)
+                    }}
+                    color="primary"
+                  />
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {row.foodname[0].toUpperCase() +
+                    row.foodname.slice(1).toLowerCase()}
+                </TableCell>
+                <TableCell align="right">{row.ENERC}</TableCell>
+                <TableCell align="right">{row.PROT}</TableCell>
+                <TableCell align="right">{row.CHOAVL}</TableCell>
+                <TableCell align="right">{row.FAT}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
-  );
+  )
 }
--- Frontend/src/components/SearchBar.js
+++ Frontend/src/components/SearchBar.js
@@ -1,17 +1,19 @@
-import * as React from "react";
-import Container from "@mui/material/Container";
-import { useState } from "react";
+import * as React from "react"
+import Container from "@mui/material/Container"
+import { useState } from "react"
 
 export default function SearchBar(props) {
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState("")
 
   return (
     <Container className="my-4">
+      <h2>Hae tietokannasta ruokaa tai raaka-ainetta</h2>
       <input
         type="text"
         onChange={(e) => setInputValue(e.target.value)}
       ></input>
-      <button onClick={(e) => props.searchCallback(inputValue)}>Etsi</button>
+
+      <button onClick={(e) => props.searchCallback(inputValue)}>Hae</button>
     </Container>
-  );
+  )
 }
