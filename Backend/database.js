e4db69f1e61061dac1516915e5e6e6feb15f3f2e
Risto-Matti Pulkkinen <rm.pulkkinen@gmail.com>
2022-11-08 18:05:58 +0200

    createFood now works with n rows

--- Backend/database.js
+++ Backend/database.js
@@ -1,14 +1,14 @@
-const dotenv = require("dotenv")
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 
-const { Pool } = require("pg")
+const { Pool } = require("pg");
 const pool = new Pool({
   user: process.env.user,
   host: process.env.host,
   database: process.env.database,
   password: process.env.password,
   port: 5432,
-})
+});
 
 async function readFoods() {
   try {
@@ -28,11 +28,11 @@ async function readFoods() {
         foods.foodname
       ORDER BY
         foodid;`
-    )
-    console.log("OK:", res.rows[0])
-    return res.rows
+    );
+    console.log("OK:", res.rows[0]);
+    return res.rows;
   } catch (err) {
-    console.log(err?.stack)
+    console.log(err?.stack);
   }
 }
 async function searchFoods(foodname) {
@@ -54,8 +54,8 @@ async function searchFoods(foodname) {
     foods.foodname
     ORDER BY CHAR_LENGTH(foodname);`,
     [`%${foodname.toUpperCase()}%`]
-  )
-  return res.rows
+  );
+  return res.rows;
 }
 
 async function readFoodId(foodid) {
@@ -67,23 +67,25 @@ async function readFoodId(foodid) {
       where foods.foodid = $1 
       and (eufdname = 'ENERC' or eufdname = 'FAT' or eufdname = 'CHOAVL' or eufdname = 'PROT' or eufdname = 'SUGAR')`,
       [foodid]
-    )
-    return res.rows
+    );
+    return res.rows;
   } catch (err) {
-    console.log(err?.stack)
+    console.log(err?.stack);
   }
 }
 
 async function createFood(meal) {
-  await pool.query(
-    `with current_meal as (insert into meals (meal_name)
-      VALUES ($1)
-      returning meal_id)
-      insert into meal_contents (meal_id, foodid, amount)
-      VALUES
-          ((select meal_id from current_meal), $2, $3)`,
-    [meal.meal_name, meal.foodid, meal.amount]
-  )
+  const valueArray = [];
+  for (const el in meal) {
+    if (meal[el].hasOwnProperty("foodid"))
+      valueArray.push(
+        `((select meal_id from current_meal), ${meal[el].foodid}, ${meal[el].amount})`
+      );
+  }
+  const mealQuery =
+    "with current_meal as (insert into meals (meal_name) VALUES ($1) returning meal_id) insert into meal_contents (meal_id, foodid, amount) VALUES " +
+    valueArray.join(", ");
+  await pool.query(mealQuery, [meal.meal_name]);
 }
 
 async function readDiary() {
@@ -111,17 +113,17 @@ async function readDiary() {
       meal_date,
       meal_name,
       amount,
-      foodname;`)
-    console.log(res.rows)
-    return res.rows
+      foodname;`);
+    console.log(res.rows);
+    return res.rows;
   } catch (err) {
-    console.log(err?.stack)
+    console.log(err?.stack);
   }
 }
 
 async function deleteMeal(mealid) {
-  await pool.query(`DELETE FROM meals WHERE meal_id = $1`, [mealid])
-  return
+  await pool.query(`DELETE FROM meals WHERE meal_id = $1`, [mealid]);
+  return;
 }
 
 module.exports = {
@@ -131,4 +133,4 @@ module.exports = {
   readDiary,
   deleteMeal,
   searchFoods,
-}
+};
